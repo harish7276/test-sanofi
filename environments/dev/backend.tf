961390f79443032053624bf9eed26b267529eed8
harish7276 <61632876+harish7276@users.noreply.github.com>
2025-04-11 17:20:43 +0530

    Update backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,8 +1,1 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "sanofi-dev-rg"
-    storage_account_name = "sanofitest"
-    container_name       = "state"
-    key                  = "terraform.tfstate"
-  }
-}
+
